70221163856971a8e8c2a53f62b7334375ada0c2
Georgi Stefanov <georgi.stefanov@uber.com>
2019-01-01 23:24:28 +0200

    Modify Main.java

--- com/fmi/mpr/hw/http/Main.java
+++ com/fmi/mpr/hw/http/Main.java
@@ -1,8 +1,10 @@
 package com.fmi.mpr.hw.http;
 
-public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Test");
+public class Main {
+	public static void main(String[] args)  throws  java.io.IOException {
+		HttpServer server = new HttpServer();
+
+		server.start();
 	}
 }
